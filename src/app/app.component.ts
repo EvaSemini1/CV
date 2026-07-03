254cf0003a5dcd86e542d58f2bf0bdbe03c5dd79
EvaSemini1 <eva.semini15@gmail.com>
2026-07-03 19:00:07 -0400

    Eva Semini Portfolio

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeroComponent } from "./components/hero/hero.component";
+import { EducationComponent } from './components/education/education.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ContactComponent } from './components/contact/contact.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HeroComponent, EducationComponent, ExperienceComponent, SkillsComponent, ContactComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'EvaSemini-Portfolio';
+}
